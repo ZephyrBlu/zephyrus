136309d4014d07d423ab1ea5c4c8251e1c68cab9
ZephyrBlu <lukejholroyd@gmail.com>
2019-01-08 17:51:55 +1300

    Implemented a basic file upload system

--- apps/upload_file/views.py
+++ apps/upload_file/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import UploadFileForm
+from django.shortcuts import render, redirect
+from .forms import ReplayFileForm
+from django.core.validators import FileExtensionValidator
+from django.views.generic.edit import FormView
+
+# class ReplayFileFormView(FormView):
+#     form_class = ReplayFileForm
+#     template_name = 'upload_file/upload_form.html'  # Replace with your template.
+#     success_url = 'home/'  # Replace with your URL or reverse().
+#
+#     def post(self, request, *args, **kwargs):
+#         form_class = self.get_form_class()
+#         form = self.get_form(form_class)
+#         files = request.FILES.getlist('file')
+#         if form.is_valid():
+#             for f in files:
+#                 file_extension_checker = FileExtensionValidator(['sc2replay'])
+#                 file_extension_checker(f.name)
+#             return self.form_valid(form)
+#         else:
+#             return self.form_invalid(form)
 
 def upload_form(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+        form = ReplayFileForm(request.POST, request.FILES)
         if form.is_valid():
-            # check file(s)
-            return # success url
+            file_extension_checker = FileExtensionValidator(['sc2replay'])
+            file_extension_checker(request.FILES['file'])
+            form.save()
+            return redirect('/home/')
     else:
-        form = UploadFileForm()
+        form = ReplayFileForm()
     return render(request, 'upload_file/upload_form.html', {'form': form})
