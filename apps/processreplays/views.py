855c379983dc058f75dee7483fa8a43e0a81d936
ZephyrBlu <lukejholroyd@gmail.com>
2019-04-27 12:59:18 +1200

    Modify bucket storage to match new replay storage structure
    Unauthenticated replays are now stored directly under the user's email account and not in a battletag folder. Still need to implement object renaming/moving to battletag folder on verification of old replays

--- apps/processreplays/views.py
+++ apps/processreplays/views.py
@@ -2,5 +2,5 @@ from .utils.replay_parser_production import main as replay_parser
 
 
 def parse_replay(file):
-    errors, player_names, meta_data, player_info, summary_info = replay_parser(file)
-    return errors, player_names, meta_data, player_info, summary_info
+    errors, meta_data, player_info, summary_info = replay_parser(file)
+    return errors, meta_data, player_info, summary_info
