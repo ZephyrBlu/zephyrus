1af6e0052c151f0402d7e7467cad9f6a5d9cc36b
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-09 13:17:04 +1200

    Simplify replay models into single Replay model
    Previously had AuthenticatedReplay and UnauthenticatedReplay models, but after removing unneccesary fields realized that the models were so similar that they were combined to reduce the number of tables and make it easier to find and store results.

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -6,36 +6,30 @@ from django.contrib.postgres.fields import JSONField
 class BattlenetAccount(models.Model):
     id = models.CharField(unique=True, null=False, max_length=100)
     battletag = models.CharField(primary_key=True, max_length=100)
-    user_account = models.ForeignKey(EmailAddress, to_field='email', on_delete=models.CASCADE)
+    user_account = models.ForeignKey(
+        EmailAddress,
+        to_field='email',
+        on_delete=models.CASCADE
+    )
     region_profiles = JSONField()
 
 
-# for replays associated with an authenticated battlenet account
-class AuthenticatedReplay(models.Model):
-    file_hash = models.CharField(max_length=200)
-    battlenet_account = models.ForeignKey(BattlenetAccount, on_delete=models.CASCADE)
-    opponent_profile_id = models.CharField(max_length=20)
-    # timeline = JSONField()
+class Replay(models.Model):
+    file_hash = models.CharField(primary_key=True, max_length=64)
+    user_account = models.ForeignKey(
+        EmailAddress,
+        to_field='email',
+        on_delete=models.CASCADE
+    )
+    battlenet_account = models.ForeignKey(
+        BattlenetAccount,
+        to_field='battletag',
+        null=True,
+        on_delete=models.CASCADE
+    )
     match_summary = JSONField()
-    user_in_game_name = models.CharField(max_length=50)
-    opponent_in_game_name = models.CharField(max_length=50)
     played_at = models.DateTimeField()
     map = models.CharField(max_length=100)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     region_id = models.IntegerField()
-
-
-# for replays uploaded to an account that are not linked
-# to any of the authenticated battlenet accounts
-# authenticated and unauthenticated replays are mutually exclusive
-class UnauthenticatedReplay(models.Model):
-    file_hash = models.CharField(max_length=200)
-    user_account = models.ForeignKey(EmailAddress, to_field='email', on_delete=models.CASCADE)
     # timeline = JSONField()
-    match_summary = JSONField()
-    player1_profile_id = models.CharField(max_length=20)
-    player2_profile_id = models.CharField(max_length=20)
-    played_at = models.DateTimeField()
-    map = models.CharField(max_length=100)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-    region_id = models.IntegerField()
