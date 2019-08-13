b1c7be06dd046567e3b7ec1df3281deb325361d7
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-14 04:40:17 +1200

    Add URL endpoint ExternalLogin and alter token endpoint
    Changed token endpoint from a class based view to the default view, since there was no need to subclass the view and change the return value.

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,18 @@
+from rest_framework.authtoken import views
 from django.urls import path
-from .views import AccountReplays, LatestReplay, CustomAuthToken # verify_replays
+from .views import (
+    ExternalLogin,
+    BattlenetAccountReplays,
+    AccountReplays,
+    LatestReplay,
+) # verify_replays
 
 
 app_name = 'api'
 urlpatterns = [
-    path('all/', AccountReplays.as_view(), name='replay_list'),
-    path('recent/', LatestReplay.as_view(), name='replay_latest'),
-    path('token/', CustomAuthToken.as_view()),
+    path('all/', BattlenetAccountReplays.as_view(), name='replay_list'),
+    path('login/', ExternalLogin.as_view(), name='external_login'),
+    # path('recent/', LatestReplay.as_view(), name='replay_latest'),
+    path('token/', views.obtain_auth_token),
     # path('verify/', verify_replays, name='verify_replays'),
 ]
