41fd7e2dcd1dd5f58f2145992569ba6add2bd87d
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-14 17:16:24 +1200

    Add CORS support for development and created logout endpoint
    Added CORS support by adding OPTIONS methods in relevant endpoints. Also configured database endpoints to read request Authentication Token and query database for the current user.

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,7 @@
 from rest_framework.authtoken import views
 from django.urls import path
 from .views import (
+    external_logout,
     ExternalLogin,
     BattlenetAccountReplays,
     AccountReplays,
@@ -12,6 +13,7 @@ app_name = 'api'
 urlpatterns = [
     path('all/', BattlenetAccountReplays.as_view(), name='replay_list'),
     path('login/', ExternalLogin.as_view(), name='external_login'),
+    path('logout/', external_logout, name='external_logout'),
     # path('recent/', LatestReplay.as_view(), name='replay_latest'),
     path('token/', views.obtain_auth_token),
     # path('verify/', verify_replays, name='verify_replays'),
