1dbe0c465dc05b46120aab47bec47ce0280ad220
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-14 17:16:24 +1200

    Add CORS support for development and created logout endpoint
    Added CORS support by adding OPTIONS methods in relevant endpoints. Also configured database endpoints to read request Authentication Token and query database for the current user.

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,10 +1,12 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseNotFound
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from apps.user_profile.models import Replay, BattlenetAccount
 from allauth.account.models import EmailAddress
+from rest_framework.authtoken.models import Token
 from .models import ReplaySerializer
 import requests
 import json
@@ -14,23 +16,46 @@ class ExternalLogin(APIView):
     authentication_classes = []
     permission_classes = []
 
+    def options(self, request):
+        response = Response({'response': 'success'})
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
     def post(self, request):
         data = json.loads(request.body)
         username = data['username']
         password = data['password']
         user = authenticate(request, username=username, password=password)
+        print(user)
+        print(user.is_authenticated)
 
         if user is not None:
             login(request, user)
+            print(user.last_login)
+            print(vars(request))
             data = {'username': username, 'password': password}
             token = requests.post(
                 url="http://127.0.0.1:8000/api/token/",
                 data=data
             )
-            return Response(token.json())
+            response = Response(token.json())
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
         else:
             response = Response({'response': 'Login details invalid'})
+            response['Access-Control-Allow-Origin'] = '*'
             return response
+
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def external_logout(request):
+    print(request.user)
+    logout(request)
+    response = Response({'response': 'Successfully logged out'})
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 # returns particular replay based on pk ID in database
@@ -52,9 +77,46 @@ class AccountReplays(APIView):
 
 # returns particular replay based on pk ID in database
 class BattlenetAccountReplays(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = ([]) #IsAuthenticated,)
+
+    def options(self, request):
+        print(vars(request))
+        print(request.user)
+        print(request.user.is_authenticated)
+        response = Response({'response': 'successful preflight'})
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
+
+    def post(self, request, format=None):
+        print(request.body)
+        request_token = json.loads(request.body)['auth']
+        print(request_token)
+        user_token = Token.objects.get(key=request_token)
+        print(user_token)
+        user = EmailAddress.objects.get(id=user_token.user_id)
+        print(vars(user))
+        print(user.last_login)
+
+        user_id = user.email
+        battle_net_id = BattlenetAccount.objects.get(user_account_id=user_id)
+
+        if not battle_net_id:
+            return HttpResponseNotFound
+
+        replay_queryset = Replay.objects.filter(battlenet_account_id=battle_net_id)
+        serialized_replays = []
+        for replay in list(replay_queryset):
+            serializer = ReplaySerializer(replay)
+            serialized_replays.append(serializer.data)
+
+        response = Response(serialized_replays)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
 
     def get(self, request, format=None):
+        print(request.user)
+        print(request.user.is_authenticated)
+        print(request.auth)
         user = request.user
         user_id = EmailAddress.objects.get(email=user.email)
         battle_net_id = BattlenetAccount.objects.get(user_account_id=user_id)
@@ -68,7 +130,9 @@ class BattlenetAccountReplays(APIView):
             serializer = ReplaySerializer(replay)
             serialized_replays.append(serializer.data)
 
-        return Response(serialized_replays)
+        response = Response(serialized_replays)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
 
 
 # returns last uploaded replay
