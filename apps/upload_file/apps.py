40c6717c9b38bdcbe9e0cdc211c89b03334ca205
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-23 13:43:51 +1200

    Remove dead code
    Removed file upload app since that functionality has been moved to the frontend app.

--- apps/upload_file/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UploadFileConfig(AppConfig):
-    name = 'apps.upload_file'
--- apps/upload_file/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'upload_file'
-urlpatterns = [
-    path('', views.upload_form, name='upload_form'),
-]
