5f7de9ac32aeb76452fa7a587019e2b530a29461
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-23 13:47:25 +1200

    Remove dead code
    Removed old profile app code

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 app_name = 'user_profile'
 urlpatterns = [
-    path('', views.overview, name='overview'),
-    path('analysis/', views.analysis, name='analysis'),
-    path('replays/', views.replays, name='replays'),
     path('authorize/', views.authentication_requests, name='authorize'),
     path('authorization/', views.need_authorization, name='require-authorization'),
-    path('new/', views.updated_profile, name='updated-profile'),
 ]
