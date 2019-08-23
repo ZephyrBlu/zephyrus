267e31d8e984584fdee50cf43de594f2593e20c9
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-23 13:49:42 +1200

    Redesign signup page

--- apps/signup/views.py
+++ apps/signup/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from allauth.account.views import SignupView
-from apps.login.views import MyLoginView
 import requests
 
 
 class MySignupView(SignupView):
     def get_context_data(self, **kwargs):
-        context = super(MySignupView, self).get_context_data(**kwargs)
+        context = {}
+        context['form'] = super(SignupView, self).get_context_data(**kwargs)['form']
         info = 'This is the sign up page'
         heading = 'Sign Up'
         title = 'Zephyrus | Sign Up'
@@ -22,24 +22,4 @@ class MySignupView(SignupView):
         username = form.cleaned_data['email']
         password = form.cleaned_data['password1']
         requests.post("http://127.0.0.1:8000/api/token/", data={"username": username, "password": password})
-        return redirect('/login/')
-
-
-# def signedup(request):
-#     info = 'Thanks for signing up to Zephyrus!'
-#     heading = 'Zephyrus: Signed Up'
-#     title = 'Zephyrus | Signed Up'
-#     context = {'info': info,
-#                'heading': heading,
-#                'title': title
-#                }
-#     return render(request, 'signup/signup_complete.html', context)
-
-
-# def email_form(request, context=None):
-#     form = SignupForm()
-#     context['form'] = form
-#     return render(request, 'signup/signup.html', context)
-#
-#
-
+        return redirect('http://localhost:5000/')
