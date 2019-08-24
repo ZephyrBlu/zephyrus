62bc7afdc52ef4345c5288a1424ebba1ff2f6bb9
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-24 18:13:55 +1200

    Move Battlenet Authorization to an API endpoint

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = 'user_profile'
 urlpatterns = [
-    path('authorize/', views.authentication_requests, name='authorize'),
-    path('authorization/', views.need_authorization, name='require-authorization'),
+    path('', views.battlenet_authorization, name='authorize'),
 ]
