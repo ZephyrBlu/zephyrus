0d408487b4dbe4e1967512ed0d010625ee8e9005
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-27 23:56:28 +1200

    Update API endpoints and Battlenet authorization

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,4 +1,4 @@
-from rest_framework.authtoken import views
+# from rest_framework.authtoken import views
 from django.urls import path, include
 from .views import (
     ExternalLogout,
@@ -6,8 +6,10 @@ from .views import (
     BattlenetAccountReplays,
     Stats,
     UploadReplays,
-    BattlenetAuthorization,
+    BattlenetAuthorizationUrl,
+    SetBattlenetAccount,
     CheckBattlenetAccount,
+    # CustomToken,
 )
 
 
@@ -16,9 +18,10 @@ urlpatterns = [
     path('all/', BattlenetAccountReplays.as_view(), name='replay_list'),
     path('login/', ExternalLogin.as_view(), name='external_login'),
     path('logout/', ExternalLogout.as_view(), name='external_logout'),
-    path('token/', views.obtain_auth_token),
+    # path('token/', CustomToken.as_view()),
     path('stats/', Stats.as_view(), name='user_stats'),
     path('upload/', UploadReplays.as_view(), name='replay_upload'),
-    path('authorize/', BattlenetAuthorization.as_view(), name='battlenet_authorization'),
-    path('check/', CheckBattlenetAccount.as_view(), name='ping-battlenet-account')
+    path('authorize/url/', BattlenetAuthorizationUrl.as_view(), name='battlenet_authorization_url'),
+    path('authorize/code/', SetBattlenetAccount.as_view(), name='set_battlenet_account'),
+    path('authorize/check/', CheckBattlenetAccount.as_view(), name='ping_battlenet_account')
 ]
