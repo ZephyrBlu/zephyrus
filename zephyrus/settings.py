aa383ac85896461c5447a9f71d9c5f029b639fa9
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-28 14:54:23 +1200

    Fix secrets for all environments

--- zephyrus/settings.py
+++ zephyrus/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .settings_secret import *
 from google.oauth2 import service_account
+from .secret.master import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
