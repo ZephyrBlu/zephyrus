1d1beb1e4720b850db3ecfda020af84015d888ce
ZephyrBlu <lukejholroyd@gmail.com>
2019-09-04 11:32:20 +1200

    Create command for updating region profile format

---
+++ apps/user_profile/management/commands/update_region_profiles.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+from apps.user_profile.models import BattlenetAccount
+
+
+class Command(BaseCommand):
+    help = 'Updates user Battlenet Account Region Profiles to a list format'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--details',
+            action='store_true',
+            help='Show the details of each account being updated'
+        )
+
+    def handle(self, *args, **options):
+        user_accounts = list(BattlenetAccount.objects.all())
+
+        for account in user_accounts:
+            for region, info in account.region_profiles:
+                if type(info['profile_id']) is not list:
+                    info['profile_id'] = [info['profile_id']]
+
+                    if options['details']:
+                        self.stdout.write(f'Updated Region {region}\n\n')
+                elif options['details']:
+                    self.stdout.write(f'Region {region} data is already in list format\n\n')
+
+            account.save()
+            if options['details']:
+                self.stdout.write(f'Updated all regions associated with: {account.battletag}\n\n')
+
+        num_accounts = len(user_accounts)
+        self.stdout.write(self.style.SUCCESS(f'Successfully updated {num_accounts} replays'))
