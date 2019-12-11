d9b4ab93d1dec286aa744f590f8bee0b54f47411
ZephyrBlu <lukejholroyd@gmail.com>
2019-12-11 23:26:56 +1300

    Add error handling for Upload endpoint

--- apps/api/views.py
+++ apps/api/views.py
@@ -422,7 +422,14 @@ class UploadReplays(APIView):
 
         replay_file.name = f'{file_hash}.SC2Replay'
         replay_file.seek(0)
-        error = process_file(replay_file, request, file_hash)
+
+        try:
+            error = process_file(replay_file, request, file_hash)
+        except Exception as e:
+            response = HttpResponseServerError(e)
+            response['Access-Control-Allow-Origin'] = FRONTEND_URL
+            response['Access-Control-Allow-Headers'] = 'authorization, content-type'
+            return response
 
         if error == 'error':
             response = HttpResponseServerError()
